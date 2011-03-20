d443c0124440101459ddd7e6f1fb7fc419b8f174
Postmodern <postmodern.mod3@gmail.com>
2011-03-20 17:20:31 -0400

    Added documentation.

--- lib/dm-is-indexed/is/indexed.rb
+++ lib/dm-is-indexed/is/indexed.rb
@@ -1,11 +1,24 @@
 module DataMapper
   module Is
     module Indexed
+      #
+      # Fired when your plugin gets included into a Model.
+      #
       def is_indexed
         extend DataMapper::Is::Indexed::ClassMethods
       end
 
       module ClassMethods
+        #
+        # Allows querying the indexed properties of a Model.
+        #
+        # @param [Integer, Object] key
+        #   The key to use when querying the Model. If the key is an Integer
+        #   it will select the nth resource of the Model.
+        #
+        # @return [DataMapper::Resource, nil]
+        #   The selected resource.
+        #
         def [](key)
           if key.kind_of?(Integer)
             super(key)
--- lib/dm/is/indexed/version.rb
+++
@@ -1,8 +0,0 @@
-module DataMapper
-  module Is
-    module Indexed
-      # dm-is-indexed version
-      VERSION = "0.1.0"
-    end
-  end
-end
